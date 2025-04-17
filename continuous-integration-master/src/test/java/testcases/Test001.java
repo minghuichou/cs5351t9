8dc835d7223c9b78434824bc5c24221f533bbb73
qwddsefsfeef <1014245700@qq.com>
2025-04-17 22:21:53 +0800

    空白测试

--- continuous-integration-master/src/test/java/testcases/Test001.java
+++ continuous-integration-master/src/test/java/testcases/Test001.java
@@ -11,7 +11,7 @@ public class Test001 {
 	 public void testA() {
 			assertEquals(3, FloorSquareRoot.floorSqrt(11));
 	 }
-
+   
 	 @Test
 	 public void testB() {
 			assertEquals(4, FloorSquareRoot.floorSqrt(20));
